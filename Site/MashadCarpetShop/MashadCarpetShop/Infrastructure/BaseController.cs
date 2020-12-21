43e1fcfbd3c89c0692b258584644ede1925a8772
zavoshsoftware <70507208+zavoshsoftware@users.noreply.github.com>
2020-12-21 18:28:11 +0330

    First push

---
+++ Site/MashadCarpetShop/MashadCarpetShop/Infrastructure/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using Helpers;
+
+namespace Khoshdast.Infrastructure
+{
+    public class BaseController : Controller
+    {
+        public BaseController()
+        {
+        }
+
+        protected override System.IAsyncResult BeginExecuteCore(System.AsyncCallback callback, object state)
+        {
+            System.Globalization.CultureInfo oCultureInfo =
+                new System.Globalization.CultureInfo("fa-IR");
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = oCultureInfo;
+            System.Threading.Thread.CurrentThread.CurrentUICulture = oCultureInfo;
+
+            //ViewBag.Name = GetUserInfo.GetUserFullName();
+            return base.BeginExecuteCore(callback, state);
+        }
+    }
+}
